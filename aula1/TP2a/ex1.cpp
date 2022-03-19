cd13ab56471fced680b06dcc69202c5b36d729b2
Eduardo Correia <up201909914@fc.up.pt>
2022-03-19 11:22:26 +0000

    Added current exercises

---
+++ aula1/TP2a/ex1.cpp
@@ -0,0 +1,73 @@
+#include "exercises.h"
+
+Labyrinth::Labyrinth(int values[10][10]) {
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            labyrinth[i][j] = values[i][j];
+    initializeVisited();
+}
+
+#include <iostream>
+
+void  Labyrinth::print() const {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            std::cout << labyrinth[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool Labyrinth::findGoal(int x, int y) {
+    if(visited[y][x] || labyrinth[y][x] == MAZE_WALL) return false;
+    visited[y][x] = true;
+    if(labyrinth[y][x] == MAZE_EXIT){
+        std::cout << "Reached the end" << std::endl;
+        return true;
+    }
+    return (findGoal(x+1,y) || findGoal(x-1,y) || findGoal(x,y-1) || findGoal(x,y+1));
+}
+
+void Labyrinth::initializeVisited() {
+    for(int i = 0; i < 10; i++)
+        for(int j = 0; j < 10; j++)
+            visited[i][j] = false;
+}
+
+/// TESTS ///
+#include <gtest/gtest.h>
+
+TEST(TP2_Ex1, testLabyrinthPossible) {
+    // This test assumes that the mazes only have a possible path to the exit
+    int lab1[10][10] ={
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,1,1,1,1,1,0,1,0,0},
+        {0,1,0,0,0,1,0,1,0,0},
+        {0,1,1,0,1,1,1,1,1,0},
+        {0,1,0,0,0,1,0,0,0,0},
+        {0,1,0,1,0,1,1,1,1,0},
+        {0,1,1,1,0,0,1,0,1,0},
+        {0,1,0,0,0,0,1,0,1,0},
+        {0,1,1,1,0,0,1,2,0,0},
+        {0,0,0,0,0,0,0,0,0,0}};
+
+    Labyrinth l1(lab1);
+    EXPECT_EQ(l1.findGoal(1,1),true);
+}
+
+TEST(TP2_Ex1, testLabyrinthImpossible) {
+    int lab1[10][10] ={
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,1,1,1,1,1,0,1,0,0},
+            {0,1,0,0,0,1,0,1,0,0},
+            {0,1,1,0,1,1,1,1,1,0},
+            {0,1,0,0,0,1,0,0,0,0},
+            {0,1,0,1,0,1,1,1,1,0},
+            {0,1,1,1,0,0,1,0,1,0},
+            {0,1,0,0,0,0,1,0,1,0},
+            {0,1,1,1,0,0,0,2,0,0},
+            {0,0,0,0,0,0,0,0,0,0}};
+
+    Labyrinth l1(lab1);
+    EXPECT_EQ(l1.findGoal(1,1),false);
+}
